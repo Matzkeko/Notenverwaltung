c5c61fcda3b9c59c0880323590c72e36a0e9d8a8
Matzkeko <68182544+Matzkeko@users.noreply.github.com>
2023-05-04 11:06:28 +0200

    Update main.java
    Window Class erstellt um dort das GUI drüber anzeigen zu lassen.

--- Notenverwaltung/src/main.java
+++ Notenverwaltung/src/main.java
@@ -4,24 +4,24 @@ import javax.swing.JFrame;
 public class main {
 
 	public static void main(String[] args) {  
-		JFrame f=new JFrame();//creating instance of JFrame  
-		          
-		JButton b=new JButton("click");//creating instance of JButton  
-		b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-		          
-		f.add(b);//adding button in JFrame  
-		          
-		f.setSize(400,500);//400 width and 500 height  
-		f.setLayout(null);//using no layout managers  
-		f.setVisible(true);//making the frame visible  
 		
-		test();
+		window();
+		
 		}  
 		
-		public static void test() {
-			System.out.println("TEst");
-			System.out.println("TEst");
-			System.out.println("Test");
+		public static void window() {
+			
+			JFrame f=new JFrame();//creating instance of JFrame  
+	          
+			JButton b=new JButton("click");//creating instance of JButton  
+			b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+			          
+			f.add(b);//adding button in JFrame  
+			          
+			f.setSize(400,500);//400 width and 500 height  
+			f.setLayout(null);//using no layout managers  
+			f.setVisible(true);//making the frame visible  
+			
 		}
 
 }
